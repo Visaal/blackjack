e3e3f3098f6d35d6896666fd993cb462f6ccc341
Visaal Patel <visaal.patel@gmail.com>
2020-12-08 15:55:22 +0000

    remove redundant break statements

--- blackjack.js
+++ blackjack.js
@@ -197,37 +197,27 @@ function getCardScore(cardValue) {
   switch (cardValue) {
     case "Ace":
       return 1;
-      break;
     case "King":
     case "Queen":
     case "Jack":
     case "ten":
       return 10;
-      break;
     case "nine":
       return 9;
-      break;
     case "eight":
       return 8;
-      break;
     case "seven":
       return 7;
-      break;
     case "six":
       return 6;
-      break;
     case "five":
       return 5;
-      break;
     case "four":
       return 4;
-      break;
     case "three":
       return 3;
-      break;
     case "two":
       return 2;
-      break;
   }
 }
 
